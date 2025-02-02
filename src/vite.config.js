ba0dc24af941fdb1b80a1ad5fae71d538721295d
Clayton Strydom <clayton@sinov8.net>
2025-02-02 12:54:07 +0200

    configure vite.config.js to allow for vue2

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue2 from '@vitejs/plugin-vue2'
 
 export default defineConfig({
     plugins: [
@@ -7,7 +8,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue2()
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js'
+        }
+    },
     server: {
         hmr: {
             host: 'localhost',
