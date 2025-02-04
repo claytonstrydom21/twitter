f9fd454d9578bb42b46919207fbc0e0d9954b958
Clayton Strydom <clayton@sinov8.net>
2025-02-04 09:03:57 +0200

    - set up routes for guest, which is for login and register - set up routes for authenticated users

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\SetUsernameController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Public routes
+Route::middleware('guest')->group(function () {
+    Route::get('/welcome', function () {
+        return view('welcome');
+    })->name('welcome');
+    // Login routes
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+
+    // Registration routes
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/set-username', [SetUsernameController::class, 'show'])->name('set-username');
+    Route::post('/set-username', [SetUsernameController::class, 'store']);
+    Route::get('/username-suggestions', [SetUsernameController::class, 'generateSuggestions']);
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
