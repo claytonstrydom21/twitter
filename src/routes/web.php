135e324a370e52050d9d299ced004095fde54a2b
Clayton Strydom <clayton@sinov8.net>
2025-02-05 15:01:54 +0200

    update login/registration process with set services and controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,31 +1,29 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\SetUsernameController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
 Route::middleware('guest')->group(function () {
-    Route::get('/welcome', function () {
+    Route::get('/', function () {
         return view('welcome');
     })->name('welcome');
-    // Login routes
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
 
-    // Registration routes
     Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
     Route::post('/register', [RegisterController::class, 'register']);
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 });
 
-// Protected routes
 Route::middleware('auth')->group(function () {
     Route::get('/set-username', [SetUsernameController::class, 'show'])->name('set-username');
     Route::post('/set-username', [SetUsernameController::class, 'store']);
     Route::get('/username-suggestions', [SetUsernameController::class, 'generateSuggestions']);
 
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
