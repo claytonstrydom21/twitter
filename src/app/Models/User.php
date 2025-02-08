5eb9c2dc397a9cd6a747ba3e31690e6a72990712
Clayton Strydom <clayton@sinov8.net>
2025-02-08 13:08:15 +0200

    add relationships for posts, followers, following, and likes to the user

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -46,4 +46,24 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'following_id', 'follower_id');
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id');
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(Post::class, 'likes');
+    }
 }
