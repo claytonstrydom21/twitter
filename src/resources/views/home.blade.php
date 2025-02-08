b500d5d05fb7f88f87b18c60fcab999b45f915be
Clayton Strydom <clayton@sinov8.net>
2025-02-08 13:09:21 +0200

    refactor feed section

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.authenticated')
 
 @section('content')
-<div class="border-b border-gray-800 px-4 py-3">
-    <h1 class="text-xl font-bold text-white">Feed</h1>
-</div>
+    <div class="border-b border-gray-800 px-4 py-3">
+        <h1 class="text-xl font-bold text-white">Feed</h1>
+    </div>
 
     <div class="px-4 py-3">
-        <create-post></create-post>
-
-        <post-feed></post-feed>
+        <feed-container
+            :current-user-id="{{ (int)auth()->id() }}"
+        ></feed-container>
     </div>
 @endsection
